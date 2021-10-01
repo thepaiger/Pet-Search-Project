5c2ed0437e01cc5ca660b6ca6c41e0b548860fb5
Paige Hoeppner <paige.a.hoeppner@gmail.com>
2021-10-01 16:01:15 -0500

    API connected and pulling data

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-console.log('hi')
+/*  
+GUIDE FOR USING cURL
+https://blog.postman.com/curl-and-postman-work-wonderfully-together/
+STACKOVERFLOW WITH LINK TO GUTHUB TO CONVERT cURL TO JS FETCH
+https://stackoverflow.com/questions/31039629/convert-curl-get-to-javascript-fetch/37801251
+LINK TO CONVERT cURL to JS FETCH
+https://kigiri.github.io/fetch/
+*/
+
+
 
 const DOMAIN = 'https://api.petfinder.com/v2';
 const CATEGORY = '/animals';
@@ -6,31 +15,44 @@ const API_KEY = 'YaPWOgbfJAuKvB8xziX6gCwdkWfonmZFLbd0iGdJWKBrDwjoTb';
 const API_SECRET = 'gkhz8FQAcHCcqgj4m9bc6tfKr6LfTZPzqFAd91U5';
 
 
+fetch("https://api.petfinder.com/v2/oauth2/token", {
+  body: `grant_type=client_credentials&client_id=${API_KEY}&client_secret=${API_SECRET}`,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  method: "POST"
+})
+  .then((results) => {
+    return results.json();
+  })
+  .then((resultsJSON) => {
+    mainFetch(resultsJSON);
+  })
+  .catch((error) => {
+    console.error(`ERROR: ${error}`);
+  });
 
-// fetch(`${DOMAIN}${API_KEY}&s=${input}`)
-//     .then((results) => {
-//       return results.json();
-//     })
-//     .then((resultsJSON) => {
-//       console.log(resultsJSON);
-
-//       renderList(resultsJSON.Search);
-
-//     })
-//     .catch((error) => {
-//       console.error(`ERROR: ${error}`);
-//     });
-
-fetch(`curl -H "Authorization: Bearer ${API_KEY}" GET ${DOMAIN}${CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}`)
+const mainFetch = (data) => {
+  fetch(`https://api.petfinder.com/v2/${CATEGORY}`, {
+    headers: {
+      Authorization: `${data.token_type} ${data.access_token}`
+    }
+  })
     .then((results) => {
       return results.json();
     })
     .then((resultsJSON) => {
       console.log(resultsJSON);
-
-      renderList(resultsJSON.Search);
-
     })
     .catch((error) => {
       console.error(`ERROR: ${error}`);
     });
+}
+
+
+
+
+
+
+
+
